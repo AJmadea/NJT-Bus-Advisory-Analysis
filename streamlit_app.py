11e05ed4b7c94cda2ee629af27f5a5a011e7a96b
Andrew <andrewmadea@gmail.com>
2021-06-04 14:28:26 -0400

    Updated the graph to make the x_range more sensible.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 import requests
 from datetime import datetime
 
@@ -63,11 +64,17 @@ def get_most_frequent(freq_table):
         if freq_table[k] == max_value:
             max_keys.append(k)
 
-    return "Bus Line(s) {} have {} appearances.".format(max_keys, max_value)
+    return "Bus Line(s) {} have {} appearances.".format(str(max_keys)[1:-1], max_value)
+
+
+def create_good_bar_graph(data):
+    data['Bus Line'] = data['Bus Line'].astype(str)
+    print(data.dtypes)
+    return px.bar(data, x='Bus Line', y='# Appearances',
+                  title="Freq of Bus Lines on NJT Bus Advisory Feed")
 
 
 if __name__ == '__main__':
-
     st.header("""NJT Bus Advisory""")
     st.subheader("""Finding The Most Common Bus Line That Has/Had Advisories""")
     freq_frame, freq_table, raw_data = get_data()
@@ -79,7 +86,10 @@ if __name__ == '__main__':
     top_buses = get_most_frequent(freq_table)
     st.write(top_buses)
 
-    fig = px.bar(freq_frame, x='Bus Line', y='# Appearances',
-                 title="Freq of Bus Lines on NJT Bus Advisory Feed")
+    freq_frame.sort_values(by=['# Appearances', "Bus Line"], inplace=True, ascending=False)
+    freq_frame.reset_index(inplace=True, drop=True)
+    fig = create_good_bar_graph(freq_frame)
+
+    print(freq_frame.head(5))
     st.plotly_chart(fig)
-    st.button(label='Fetch Info')
+    st.button(label='Update Info')
