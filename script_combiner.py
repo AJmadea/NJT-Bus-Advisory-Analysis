719154be1a08a9cfb93c3ff2011ca1be7e4a939b
Andrew <andrewmadea@gmail.com>
2021-06-11 22:38:24 -0400

    Implemented the Logger

--- script_combiner.py
+++ script_combiner.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 from datetime import datetime
+from Logger import Logger
 
 
 def convert_to_datetime(string, month_dict):
@@ -30,44 +31,63 @@ def update_info_file(now, new_shape):
     info.to_csv("C:/Users/Andrew/Desktop/njt_bus_adv_data/info.csv")
 
 
-if __name__=="__main__":
-    month_dict = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06',
-                  'Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'}
-    base = "C:/Users/Andrew/Desktop/njt_bus_adv_data/auto/"
-    dirs = os.listdir(base)
+if __name__ == "__main__":
+    l = Logger(path="C:/Users/Andrew/Desktop/njt_bus_adv_data/logs/")
 
-    frames = []
-    for file_name in dirs:
-        temp = pd.read_csv(base + file_name, index_col=0)
-        print(base+file_name)
-        for i in temp.index:
-            dt = str(convert_to_datetime(temp.loc[i, 'DATE_TIME'], month_dict))
-            temp.loc[i, 'DATE_TIME'] = dt
-            bus = str(temp.loc[i, 'BUS'])
+    try:
+        month_dict = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06',
+                      'Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'}
+        base = "C:/Users/Andrew/Desktop/njt_bus_adv_data/auto/"
+        dirs = os.listdir(base)
 
-            temp.loc[i, 'BUS_DATETIME'] = bus + " " + dt
-            temp.loc[i, 'DESCRIPTION'] = temp.loc[i, 'DESCRIPTION'].lstrip()
-        frames.append(temp)
-    all_data = pd.concat(frames)
-    print(all_data.head())
-    all_data.drop_duplicates(inplace=True)
+        l.log("Amount of files in the auto folder: " + str(len(dirs)))
 
-    for e in dirs:
-        os.remove(base+e)
-
-    base2 = "C:/Users/Andrew/Desktop/njt_bus_adv_data/combined/"
-    files = os.listdir(base2)
-    now = datetime.now()
-    if len(files) < 1:
-        all_data.to_csv(base2 + "current_total_{}.csv".format(now.strftime("%y%m%d_%H%M%S")))
-    else:
         frames = []
-        new_data = combine_files(base2)
-        data = pd.concat([new_data, all_data])
-        data.drop_duplicates(inplace=True)
-        data.to_csv(base2 + "current_total_{}.csv".format(now.strftime("%y%m%d_%H%M%S")))
+        for file_name in dirs:
+            temp = pd.read_csv(base + file_name, index_col=0)
+            print(base+file_name)
+            for i in temp.index:
+                temp_date = temp.loc[i, 'DATE_TIME']
+                dt = str(convert_to_datetime(temp_date, month_dict))
+                temp.loc[i, 'DATE_TIME'] = dt
+                bus = str(temp.loc[i, 'BUS'])
 
-        update_info_file(now, data.shape[0])
+                temp.loc[i, 'BUS_DATETIME'] = bus + " " + dt
+                temp_string = temp.loc[i, 'DESCRIPTION'].strip()
+                temp_string = temp_string.replace("&amp;", "")
+                temp.loc[i, 'DESCRIPTION'] = temp_string
+            frames.append(temp)
+        all_data = pd.concat(frames)
+        print(all_data.head())
+        all_data.drop_duplicates(inplace=True)
 
-        for e in files:
-            os.remove(base2+e)
+        l.log("Amount of rows in the combined auto csv " + str(all_data.shape[0]))
+
+        for e in dirs:
+            os.remove(base+e)
+
+        base2 = "C:/Users/Andrew/Desktop/njt_bus_adv_data/combined/"
+        files = os.listdir(base2)
+        l.log("There are {} files in the combined folder".format(len(files)))
+        now = datetime.now()
+        if len(files) < 1:
+            all_data.to_csv(base2 + "current_total_{}.csv".format(now.strftime("%y%m%d_%H%M%S")))
+        else:
+            frames = []
+            new_data = combine_files(base2)
+            data = pd.concat([new_data, all_data])
+            data.drop_duplicates(inplace=True)
+
+            data.to_csv(base2 + "current_total_{}.csv".format(now.strftime("%y%m%d_%H%M%S")))
+
+            update_info_file(now, data.shape[0])
+            l.log("Updating the info.csv... with {} rows".format(data.shape[0]))
+            for e in files:
+                os.remove(base2+e)
+
+        l.log_with_datetime("Flushing...")
+    except Exception as err:  # Use specific exception
+        l.log(err)
+    finally:
+
+        l.flush()
