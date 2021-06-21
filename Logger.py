a9880339af158f6de3b052dc92e2fe4d2462b7d5
Andrew <andrewmadea@gmail.com>
2021-06-21 17:04:32 -0400

    Added param to constructor.  Max files is the maximum amount of files the folder can contain.  If the logger wants to create a new log file, then it will delete the oldest file.

--- Logger.py
+++ Logger.py
@@ -4,22 +4,28 @@ from datetime import datetime
 
 class Logger:
 
-    def __init__(self, path, list_of_strings=[]):
+    def __init__(self, path, max_files=10, list_of_strings=[]):
+        self.max_files = max_files
         self.list_of_strings = list_of_strings
         self.path = path
 
     def log(self, string):
         self.list_of_strings.append("{}\n".format(string))
 
-    def log_now(self, string):
-        self.list_of_strings.append(string + "{}\n".format(datetime.now().strftime("%Y %b %d Time: %H:%M:%S")))
+    def log_with_datetime(self, string):
+        self.list_of_strings.append("{} {}\n".format(string, datetime.now().strftime("%Y %b %d Time: %H:%M:%S")))
 
     def flush(self):
         directories = os.listdir(self.path)
         directories.sort()
-        if len(directories) > 5:
-            os.remove(self.path+directories[-1])
-        now = datetime.now()
 
+        if len(directories) >= self.max_files:
+            removing = "{}{}".format(self.path, directories[0])
+            # print(removing)
+            self.log("Removing File From Folder: {}".format(removing))
+            os.remove(removing)
+
+        now = datetime.now()
+        self.log_with_datetime("Creating log file...")
         with open(self.path+"log_{}.txt".format(now.strftime("%y%m%d_%H%M%S")), "w") as writer:
             writer.writelines(self.list_of_strings)
